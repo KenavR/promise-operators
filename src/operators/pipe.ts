05e500f3be5ac1dc194332de358fbd0ffed3b693
KenavR <kenavr@gmail.com>
2019-06-18 13:55:32 +0200

    Added pipe operator

--- src/operators/pipe.ts
+++ src/operators/pipe.ts
@@ -1,1 +1,13 @@
-export function pipe(...operators: any) {}
+export function pipe(
+  this: any,
+  ...operators: any
+): (data: any) => Promise<any> {
+  function reducer(this: any, acc: any, op: any) {
+    return op.call(this, acc);
+  }
+
+  const pipeContext = { ...this, piped: true };
+  return (data: any) => {
+    return operators.reduce(reducer.bind(pipeContext), data);
+  };
+}
