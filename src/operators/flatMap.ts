f7109be84693481f6fbbc986368dba5bd6effb57
KenavR <kenavr@gmail.com>
2019-06-20 19:33:06 +0200

    Updated flatMap to support new pipes

--- src/operators/flatMap.ts
+++ src/operators/flatMap.ts
@@ -1,16 +1,30 @@
-import { map } from "./map";
+import { map } from './map';
+import { PipedContext } from '../context';
+import { isPiped } from '../utils';
 
-interface MapperFunc<U> {
-  (value: any, index?: number, array?: any[]): U;
+interface FlatMapperFunc<U> {
+  (value: any, index?: number, array?: any[]): Promise<U>;
 }
 
-export function flatMap(mapper: MapperFunc<any>) {
-  return (data: any) => {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(map(mapper) as any)).then(res => res);
+export function flatMap(
+  mapper: FlatMapperFunc<any>
+): (data: any) => Promise<any> | any {
+  return function doWork(this: PipedContext, data: any): any | Promise<any> {
+    function apply(data: any): Promise<any> {
+      if (Array.isArray(data)) {
+        return Promise.all(data.map(map(mapper) as any)).then(res => res);
+      }
+
+      return map(mapper)(data).then((res: any) => res);
     }
 
-    return map(mapper)(data).then((res: any) => res);
+    if (isPiped(this) && this.chain) {
+      const operator = this.chain.shift();
+      const result$ = apply(data);
+      return !!operator ? result$.then(operator.bind(this)) : result$;
+    }
+
+    return apply(data);
   };
 }
 
--- test/operators/flatMap.test.ts
+++ test/operators/flatMap.test.ts
@@ -1,6 +1,57 @@
+import { flatMap, pipe } from '../../src';
+
+interface Fullname {
+  firstname: string;
+  lastname: string;
+}
+
+function addOne(val: number): Promise<number> {
+  return Promise.resolve(val + 1);
+}
+
+function getLastname(name: Fullname): Promise<string> {
+  return Promise.resolve(name.lastname);
+}
+
 describe('flatMap', () => {
-  it('should handle two synchronous functions', () => {
-    const expected = 120;
-    expect(120).toEqual(expected);
+  it('should handle array', () => {
+    const initial = [1, 2, 3];
+    const expected = [2, 3, 4];
+
+    expect(flatMap(addOne)(initial)).resolves.toEqual(expected);
+  });
+
+  it('should handle object', () => {
+    const expected = 'Doe';
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: expected
+    };
+
+    expect(flatMap(getLastname)(initial)).resolves.toEqual(expected);
+  });
+
+  it('should be pipeable', () => {
+    const initial = [1, 2, 3];
+    const expected = [2, 3, 4];
+    const pipeContext = { piped: true, chain: [] };
+
+    const pipedFlatMap = flatMap(addOne).bind(pipeContext);
+    const result = pipedFlatMap(initial);
+
+    expect(result).resolves.toEqual(expected);
+  });
+
+  it('should work inside pipe', () => {
+    const initial = [1, 2, 3];
+    const expected = [3, 4, 5];
+    const chain = Promise.resolve(initial).then(
+      pipe(
+        flatMap(addOne),
+        flatMap(addOne)
+      )
+    );
+
+    expect(chain).resolves.toEqual(expected);
   });
 });
