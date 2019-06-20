fd671ab0d2464b34b51954abc27b23d831670d0c
KenavR <kenavr@gmail.com>
2019-06-20 16:22:05 +0200

    Implemented filter

--- src/operators/filter.ts
+++ src/operators/filter.ts
@@ -1,1 +1,57 @@
-export function filter() {}
+import { isPiped } from '../utils';
+import { PipedContext } from '../context';
+
+export function filter<T>(
+  condition: (data: T) => boolean,
+  error?: Error | string
+): (data: any) => any | Promise<any> {
+  return function doWork(this: PipedContext, data: any): any | Promise<any> {
+    function getResult(data: any): Array<T> | boolean {
+      if (Array.isArray(data)) {
+        return data.filter(condition);
+      }
+      return condition(data);
+    }
+
+    function throwError(error?: Error | string) {
+      if (error) {
+        return Promise.reject(
+          error instanceof Error ? error : new Error(error)
+        );
+      }
+
+      return Promise.reject(new Error('Condition is not met!'));
+    }
+
+    function handleValidObject(this: PipedContext, data: T) {
+      if (isPiped(this) && this.chain) {
+        const operator = this.chain.shift();
+        return !!operator ? operator.call(this, data) : data;
+      }
+
+      return Promise.resolve(data);
+    }
+
+    const result = getResult(data);
+
+    // Object does not meet condition
+    if (result === false) {
+      return throwError(error);
+    }
+
+    // Object meets condition
+    if (result === true) {
+      return handleValidObject.call(this, data);
+    }
+
+    // Filtered Array
+    if (result) {
+      if (isPiped(this) && this.chain) {
+        const operator = this.chain.shift();
+        return !!operator ? operator.call(this, result) : result;
+      }
+    }
+
+    return Promise.resolve(result);
+  };
+}
--- test/operators/filter.test.ts
+++ test/operators/filter.test.ts
@@ -1,6 +1,177 @@
+import { map, pipe, filter } from '../../src';
+
+interface Fullname {
+  firstname: string;
+  lastname: string;
+}
+
 describe('filter', () => {
-  it('should handle two synchronous functions', () => {
-    const expected = 120;
-    expect(120).toEqual(expected);
+  it('should handle array', () => {
+    const initial = [1, 2, 3, 4, 5];
+    const expected = [1, 2];
+
+    expect(filter((v: number) => v < 3)(initial)).resolves.toEqual(expected);
+  });
+
+  it('should handle object, resolve', () => {
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+
+    expect(
+      filter((v: Fullname) => v.firstname === initial.firstname)(initial)
+    ).resolves.toEqual(initial);
+  });
+
+  it('should handle object, reject', () => {
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+
+    expect(
+      filter((v: Fullname) => v.firstname === '')(initial)
+    ).rejects.toThrow();
+  });
+
+  it('should handle object, reject with specific message', () => {
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+
+    const specificMessage = 'Specific Message';
+
+    expect(
+      filter((v: Fullname) => v.firstname === '', specificMessage)(initial)
+    ).rejects.toThrow(specificMessage);
+  });
+
+  it('should handle object, reject with specific error', () => {
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+
+    const specificError = new Error('My error');
+
+    expect(
+      filter((v: Fullname) => v.firstname === '', specificError)(initial)
+    ).rejects.toThrow(specificError);
+  });
+
+  it('should be pipeable, array - chain', () => {
+    const calledMap = jest.fn(map(v => v + 1));
+
+    const initial = [1, 2, 3, 4, 5];
+    const expected = [2, 3];
+    const pipeContext = {
+      piped: true,
+      chain: [calledMap]
+    };
+
+    const pipedFilter = filter((v: number) => v < 3).bind(pipeContext);
+    const result = pipedFilter(initial);
+
+    // console.log('RESULT:::: ', result);
+    expect(result).toEqual(expected);
+    expect(calledMap).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be pipeable, object - break chain', () => {
+    const uncalledMap = jest.fn(map((fn: Fullname) => fn.firstname));
+
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    const pipeContext = {
+      piped: true,
+      chain: [uncalledMap]
+    };
+
+    const pipedFilter = filter((fn: Fullname) => fn.firstname === '').bind(
+      pipeContext
+    );
+    const result = pipedFilter(initial);
+
+    expect(result).rejects.toThrow();
+    expect(uncalledMap).not.toHaveBeenCalled();
+  });
+
+  it('should be pipeable, object - chain', () => {
+    const calledMap = jest.fn(map((fn: Fullname) => fn.firstname));
+
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    const pipeContext = {
+      piped: true,
+      chain: [calledMap]
+    };
+
+    const pipedFilter = filter(
+      (fn: Fullname) => fn.firstname === initial.firstname
+    ).bind(pipeContext);
+    const result = pipedFilter(initial);
+
+    expect(result).toEqual(initial.firstname);
+    expect(calledMap).toHaveBeenCalledTimes(1);
+  });
+
+  it('should work inside pipe, array', async () => {
+    const calledMap = jest.fn(map(v => v + 1));
+
+    const initial = [1, 2, 3];
+    const expected = [] as number[];
+    const chain = Promise.resolve(initial).then(
+      pipe(
+        filter((v: number) => v > 100),
+        calledMap
+      )
+    );
+
+    await expect(chain).resolves.toEqual(expected);
+    expect(calledMap).toHaveBeenCalledTimes(1);
+  });
+
+  it('should work inside pipe, object', async () => {
+    const calledMap = jest.fn(map((fn: Fullname) => fn.firstname));
+
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+
+    const chain = Promise.resolve(initial).then(
+      pipe(
+        filter((fn: Fullname) => fn.firstname === initial.firstname),
+        calledMap
+      )
+    );
+
+    await expect(chain).resolves.toEqual(initial.firstname);
+    expect(calledMap).toHaveBeenCalledTimes(1);
+  });
+
+  it('should work inside pipe, object - break chain', async () => {
+    const uncalledMap = jest.fn(map((fn: Fullname) => fn.firstname));
+
+    const initial: Fullname = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+
+    const chain = Promise.resolve(initial).then(
+      pipe(
+        filter((fn: Fullname) => fn.firstname === ''),
+        uncalledMap
+      )
+    );
+
+    await expect(chain).rejects.toThrow();
+    expect(uncalledMap).not.toHaveBeenCalled();
   });
 });
